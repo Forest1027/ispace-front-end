9eff09cd5524f518c953fddf4f3e373e16e5dd65
Forest1027 <azureforestryan@gmail.com>
2021-07-28 20:40:24 +0100

    article management create/update

--- src/components/Articles/ArticleDetail.js
+++ src/components/Articles/ArticleDetail.js
@@ -10,6 +10,15 @@ import useConstructor from "../../hooks/useConstructor";
 import axios from "axios";
 import { createEditorStateWithText } from '@draft-js-plugins/editor';
 import { stateFromHTML } from 'draft-js-import-html'
+import { useHistory } from 'react-router-dom';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { useOktaAuth } from '@okta/okta-react';
+import { convertFromHTML, ContentState, convertToRaw } from "draft-js";
+import { convertFromRaw } from "draft-js";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -55,6 +64,9 @@ const ArticleDetail = (props) => {
     const [content, setContent] = useState("");
     const [editorState, setEditorState] = useState(createEditorStateWithText(""));
     const [isLoading, setIsLoading] = useState(true);
+    const history = useHistory();
+    const [open, setOpen] = useState(false);
+    const { authState, oktaAuth } = useOktaAuth();
 
     useConstructor(() => {
         axios.get(`http://localhost:8080/articleManagement/v1/articles/${props.location.state.articleId}`)
@@ -81,10 +93,88 @@ const ArticleDetail = (props) => {
             setDescription(e.target.value);
         }
     }
-    const onEditorChangeHandler = (editorState) => {
-        setEditorState(editorState);
-        setContent(JSON.stringify(stateToHTML(editorState.getCurrentContent())));
+
+    const onCreateHandler = () => {
+        const accessToken = `Bearer ${oktaAuth.getAccessToken()}`;
+        axios.post(
+            `http://localhost:8080/articleManagement/v1/articles`,
+            {
+                title: title,
+                description: description,
+                content: content
+            },
+            {
+                headers:
+                {
+                    'Authorization': accessToken,
+                }
+            })
+            .then(res => {
+                history.push("/articleManagement");
+            })
+            .catch(err => {
+
+            });
     }
+
+    const onEditorChangeHandler = (editorState) => {
+        console.log("update")
+        console.log(JSON.stringify(stateToHTML(editorState.getCurrentContent())))
+        setEditorState(editorState);
+        setContent(stateToHTML(editorState.getCurrentContent()));
+
+    }
+
+    const onUpdateHandler = () => {
+        const accessToken = `Bearer ${oktaAuth.getAccessToken()}`;
+        axios.put(
+            `http://localhost:8080/articleManagement/v1/articles`,
+            {
+                id: props.location.state.articleId,
+                title: title,
+                description: description,
+                content: content
+            },
+            {
+                headers:
+                {
+                    'Authorization': accessToken,
+                }
+            })
+            .then(res => {
+                history.push("/articleManagement");
+            })
+            .catch(err => {
+
+            })
+    }
+
+    const onCancelHandler = () => {
+        setOpen(true);
+    }
+
+    const confirmCancel = () => {
+        history.push("/articleManagement");
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const leavePageHandler = (e) => {
+        e = e || window.event;
+
+        // For IE and Firefox
+        if (e) {
+            e.returnValue = 'Leaving the page';
+        }
+
+        // For Safari
+        return 'Leaving the page';
+    }
+
+    window.onbeforeunload = leavePageHandler;
+
     return (
         <div>
             {isLoading ?
@@ -92,9 +182,9 @@ const ArticleDetail = (props) => {
                 (<div>
                     <form className={classes.root} noValidate autoComplete="off">
                         <div>
-                            {props.location.state.mode === constants.MODE_CREATE ? <Button variant="outlined" color="secondary" className={classes.button} >Create</Button> : null}
-                            {props.location.state.mode === constants.MODE_EDIT ? <Button variant="outlined" color="secondary" className={classes.button} >Update</Button> : null}
-                            <Button variant="outlined" color="primary" className={classes.button}>Cancel</Button>
+                            {props.location.state.mode === constants.MODE_CREATE ? <Button variant="outlined" color="secondary" className={classes.button} onClick={onCreateHandler}>Create</Button> : null}
+                            {props.location.state.mode === constants.MODE_EDIT ? <Button variant="outlined" color="secondary" className={classes.button} onClick={onUpdateHandler}>Update</Button> : null}
+                            <Button variant="outlined" color="primary" className={classes.button} onClick={onCancelHandler}>Cancel</Button>
                         </div>
                         <div>
                             <TextField id="standard-basic" label="Title" fullWidth name="title" onChange={onChangeHandler} value={title} />
@@ -118,7 +208,27 @@ const ArticleDetail = (props) => {
 
                     {props.location.state.articleId}
                 </div>)}
-
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"The change is not saved yet"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Cancel update or create? The change won't be saved.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        No
+                    </Button>
+                    <Button onClick={confirmCancel} color="secondary" autoFocus>
+                        Yes
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <div>
                 {props.location.state.author}
             </div>
